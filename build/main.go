59986428d8d52d10baa6c5e026df1b35aaa98611
Tommy Calvy <thomaslcalvy@gmail.com>
2018-12-06 21:01:20 -0500

    hsdfsdf

--- build/main.go
+++ build/main.go
@@ -8,6 +8,9 @@ import (
 	"google.golang.org/appengine"
 	"net/http"
 	"html/template"
+	"log"
+	"io/ioutil"
+	"url"
 )
 
 var tpl *template.Template
@@ -16,15 +19,38 @@ func init() {
 	tpl = template.Must(template.ParseGlob("templates/*"))
 }
 
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-        if r.URL.Path != "/" {
-                http.Redirect(w, r, "/", http.StatusFound)
-                return
-        }
-        tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	if r.URL.Path != "/" {
+		http.Redirect(w, r, "/", http.StatusFound)
+    return
+  }
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
 }
 
+func eventSearch(w http.ResponseWriter, r *http.Request) {
+	rootUrl := "https://app.ticketmaster.com/discovery/v2/events.json?"
+	apiKey := "apikey=YoEXzVKdkKvHHtivISGCzEJrrtfMYGxE"
+	if r.Method == "POST" {
+		searchInput := r.Form["userSearchTerms"]
+		keyword := "keyword=" + url.PathEscape(searchInput)
+		url := rootUrl + searchInput + keyword
+		resp, err := http.Get(url)
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			data, _ := ioutil.ReadAll(response.Body)
+			log.Println(string(data))
+			tpl.ExecuteTemplate(w, "searchResults.gohtml", data)
+		}
+	}
+
+
+}
+
+
 func main() {
+	http.HandleFunc("/eventSearch", eventSearch)
 	http.HandleFunc("/", indexHandler)
 	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
 	appengine.Main()
